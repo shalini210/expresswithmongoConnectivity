1011a232ff095e6a345c090e74de9a5c9d8b9fe1
shalini <s.rathore43@gmail.com>
2024-04-26 13:25:18 +0530

    fullapi

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -3,9 +3,10 @@ const userModel = require("../models/usermodel");
 
 exports.insertUser = (req,res)=>{
     const newuser = new userModel(
-        {uname:req.body.uname,
+        {
+            uname:req.body.uname,
             pwd:req.body.pwd}
-            )
+            );            
     newuser.save()
 .then(()=>{console.log("record inseted ");
 res.send("user inserted ");
@@ -13,4 +14,53 @@ res.send("user inserted ");
 .catch(()=>
 {console.log("Record not inserted")
     res.send("user not inserted")});
+}
+
+
+exports.updateUser = (req,res)=>{
+    const condition = { uname:req.body.uname};
+    const newdata =  {    
+            pwd:req.body.pwd}
+          
+            userModel.updateMany(condition, newdata)
+// userModel.findOneAndUpdate(condition, newdata)
+.then((d)=>{
+    console.log("record updatged");
+    console.log(d);
+res.send("user updated ");
+        })
+.catch(()=>
+{console.log("Record notupdate")
+    res.send("user not updated")});
+}
+
+exports.deleteUser = (req,res)=>
+{
+    userModel.deleteOne({uname:req.body.uname})
+    .then((d)=>{
+        console.log(d)
+        console.log("record deleted");
+        if(d.deletedCount>0)
+        {
+        res.send("record deleted");
+        }
+        else
+        {
+            res.send("record not found")
+        }
+    })
+    .catch(()=>
+    {console.log("record not deleted")})
+}
+
+exports.getUsers=async (req,res)=>
+{
+    const data =await  userModel.find()
+    res.send(data);
+}
+exports.getUser = async(req,res)=>
+{
+    console.log(req.query)
+    const data = await userModel.find({uname:req.query.uname});
+    res.send(data);
 }
--- routers/userrouter.js
+++ routers/userrouter.js
@@ -3,19 +3,27 @@ const router = express.Router();
 const usercontroller = require("../controllers/usercontroller")
 router.get("/",(req,res)=>
 {
-res.send("we will show users here")
+// res.send("we will show users here")
+usercontroller.getUsers(req,res);
+});
+router.get("/showuser",(req,res)=>
+{
+
+usercontroller.getUser(req,res);
 });
 router.post("/",(req,res)=>
 {
+    console.log("inside post");
 usercontroller.insertUser(req,res);
 });
+
 router.put("/",(req,res)=>
 {
-
+    usercontroller.updateUser(req,res);
 });
 router.delete("/",(req,res)=>
 {
-
+    usercontroller.deleteUser(req,res);
 });
 
 module.exports = router;
